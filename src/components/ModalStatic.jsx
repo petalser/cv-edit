ab3e3c75c106798a00c7f9beed72845cbea753e1
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-03-28 15:13:43 +0100

    ModalStatic body

--- src/components/ModalStatic.jsx
+++ src/components/ModalStatic.jsx
@@ -1,16 +1,50 @@
 import { Modal, Button } from "react-bootstrap";
+import { signalData } from "../signals/data";
+import { useState } from "react";
 
 const ModalStatic = ({ show, onHide, id, modalType }) => {
+  //chunk of global data object
+  const dataChunk = signalData.value[id];
+
+  const [data, setData] = useState(dataChunk);
+
   console.log("modal static fired");
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header closeButton>
         <Modal.Title>
-          {/* {modalType === "json" ? "Edit JSON" : "Edit module"} */}
-          Title
+          {modalType === "json" ? "Edit JSON" : "Edit module"}
         </Modal.Title>
       </Modal.Header>
-      <Modal.Body></Modal.Body>
+      <Modal.Body>
+        <div className="row">
+          <div className="col">
+            {Object.keys(dataChunk).map((item, index) => (
+              <label
+                htmlFor={item}
+                className="form-label d-flex justify-content-end"
+                key={index}
+              >
+                {data[item].description}
+              </label>
+            ))}
+          </div>
+          <div className="col">
+            {Object.keys(dataChunk).map((item, index) => (
+              <input
+                type="text"
+                id={item}
+                className="form-control"
+                value={data[item].value}
+                onChange={(e) =>
+                  setData({ ...data, [data[item].value]: e.target.value })
+                }
+                key={index}
+              />
+            ))}
+          </div>
+        </div>
+      </Modal.Body>
       <Modal.Footer>
         <Button variant="secondary" onClick={onHide}>
           Cancel
