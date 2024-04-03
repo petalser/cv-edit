cb0a1bc1625adbcac4f005642a1e6dbdf253dfdd
Alex Petrychuk <93341599+petalser@users.noreply.github.com>
2024-04-03 12:47:39 +0200

    Rebuilt with "Toast" and proper icon

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -1,28 +1,20 @@
-import { isTooltipEnabled } from "../signals/states";
-import { useSignals } from "@preact/signals-react/runtime";
-import "../styles/Tooltip.css";
+import { Toast } from "react-bootstrap";
+import { CaretLeftFill } from "react-bootstrap-icons";
 
 const Tooltip = () => {
-  useSignals();
   return (
-    <div className="c-tooltip">
-      <p className="c-text-upper">
-        <span className="faded">H</span>OVER HERE!
-      </p>
-
-      <button
-        className="symbol close"
-        onClick={() => {
-          isTooltipEnabled.value = false;
-        }}
-      >
-        &times;
-      </button>
-      <p className="symbol arrow">&lt;</p>
-      <p className="c-text-lower">
-        Hover over left border of screen to see tools
-      </p>
-    </div>
+    <Toast bg="dark" className="text-white  opacity-75 position-absolute">
+      <Toast.Header className="bg-dark text-white">
+        <strong>
+          <span className="opacity-75">H</span>
+          OVER HERE!
+        </strong>
+      </Toast.Header>
+      <Toast.Body className="d-flex">
+        <CaretLeftFill color="gray" size={55} />
+        <p>Hover over left border of screen to see tools</p>
+      </Toast.Body>
+    </Toast>
   );
 };
 
